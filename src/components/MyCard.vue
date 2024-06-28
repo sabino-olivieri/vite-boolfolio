e3a5c2f1f944d2f68b526199f0b7de408440548a
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-06-28 18:37:21 +0200

    feat: created homaPage with card

---
+++ src/components/MyCard.vue
@@ -0,0 +1,49 @@
+<template>
+
+        <div class="card ms_card h-100">
+            <img :src="project.image ? `${baseUrl}${project.image}` : 'https://placehold.co/800?text=Immagine+non+inserita&font=roboto'"
+                class="card-img-top" alt="...">
+            <div class="card-body d-flex flex-column justify-content-center">
+                <h5 class="card-title">{{ project.title }}</h5>
+                <div class="d-flex gap-1 my-1">
+                    <span class="badge" :style="{ backgroundColor: technologies.color }" v-for="technologies in project.technologies">
+                        {{ technologies.name }}
+                    </span>
+                </div>
+
+                <p class="card-text" v-if="project.description">{{ project.description }}</p>
+                <a :href="project.site_url" class="text-decoration-none" target="_blank" v-if="project.site_url">{{ project.site_url }}</a>
+            </div>
+        </div>
+    
+</template>
+
+<script>
+export default {
+    props: {
+        project: Object,
+    },
+    data() {
+        return {
+            baseUrl: 'http://127.0.0.1:8000/storage/'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.ms_card {
+    border: 0;
+
+    .badge {
+        padding: 10px;
+        box-sizing: content-box;
+        text-shadow: 0px 1px black,
+            1px 0px black,
+            0px -1px black,
+            -1px 0px black;
+
+    }
+
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,39 @@
 <template>
-    <div>
-        <h1>Home</h1>
+    <div class="container my-3">
+        <div class="row g-3 justify-content-center">
+            <div class="col-12 text-center">
+                <h2>Lista progetti</h2>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-4" v-for="project in dataProjects.data">
+                <MyCard :project="project"/>
+            </div> 
+        </div>
     </div>
 </template>
 
 <script>
+import axios from "axios";
+import MyCard from '../components/MyCard.vue';
+
     export default {
-        
+    components: { MyCard },
+        data() {
+            return {
+                per_page: "",
+                dataProjects: [],
+            }
+        },
+        created() {
+            this.getProjects();
+        },
+        methods: {
+            getProjects(){
+                axios.get(`http://127.0.0.1:8000/api/projects/${this.per_page}`).then((resp)=>{
+                    this.dataProjects = resp.data.response;
+                });
+            }
+        }
     }
 </script>
 
